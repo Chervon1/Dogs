72ce9dc97933d149e474e53e381d87e30d025e91
Chervon Dawkins <chervon_dawkins@bloomfield.edu>
2021-11-02 13:48:04 -0400

    Add files via upload

---
+++ firebase.mjs
@@ -0,0 +1,65 @@
+// OUR firebase File
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged } 
+            from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
+import { getFirestore, addDoc, getDocs, collection } 
+            from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvk2Gs-_NTPadVvN03EmpwVu40FUW_ogA",
+  authDomain: "dogbreed-b1789.firebaseapp.com",
+  projectId: "dogbreed-b1789",
+  storageBucket: "dogbreed-b1789.appspot.com",
+  messagingSenderId: "844401602012",
+  appId: "1:844401602012:web:5e785176b0d8e01dd1f319"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+window.auth = getAuth();
+const db = getFirestore(app);
+
+// exposed functionality
+
+window.register = function(email, passsword){
+    return createUserWithEmailAndPassword (auth, email, password);
+}
+
+window.isLoggedIn = function(){
+    return auth.currentUser !== null;
+}
+
+window.login = function(email,password){
+    return signInWithEmailAndPassword(auth, email, password);
+}
+
+window.register = function(email, passsword){
+    return createUserWithEmailAndPassword (auth, email, password);
+}
+
+window.logout = function(){
+    auth.signOut();
+}
+
+window.onLogin = function( f ){
+    onAuthStateChanged(auth, user => {
+        f( user );
+    });
+}
+
+//////////////////////////////////////////////
+// exposed functionality for db
+window.addComment = function(comment){
+    return addDoc( collection(db, "comments"), {comment} );
+}
+
+window.forEachComment = async function( f ){
+    var docs = await getDocs( collection(db, "comments") );
+    console.log(docs);
+    docs.forEach( doc => f(doc.data()) );
+}
---
+++ main.js
@@ -0,0 +1,97 @@
+const $ = document.querySelector.bind(document)
+
+function showImage(event){
+    var breed = this.innerText;
+    var priorSelected = $('selected');
+    if(priorSelected){
+        priorSelected.classList = '';
+    }
+    this.classList.add('selected');
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(r=>r.json())
+    .then( data =>{
+       // console.log( data.message)
+       $('#dog').src = data.message;
+    });
+}
+
+function createButton(txt){
+    var btn = document.createElement('button');
+    btn.innerText = txt;
+    $('#button').appendChild(btn);
+    btn.onclick = showImage;
+}
+
+function createComment( commentDoc ){
+    var div = document.createElement('div');
+    div.innerText = commentDoc.comment;
+    $('#comments').appendChild(div);
+    div.className = 'comment';
+}
+
+
+window.onload = function(){
+    
+    // add dog breen buttons
+    // $('.buttons').innerHTML = 'hey now'
+    this.fetch('https://dog.ceo/api/breeds/list/all')
+    .then(r=>r.json())
+    .then(data => {
+        // console.log ( data )
+        Object.keys( data.message )
+            .forEach( createButton );
+    })
+
+    onLogin( user => {
+        if(user){
+            //user just logged in
+            $('#addCommentDiv').style.display = 'block';
+            $('#loginDiv').style.display = 'none';
+            $('#signupDiv').style.display = 'none';
+        }else{
+            //user just logged out
+            $('#loginDiv').style.display = 'block';
+            $('#addCommentDiv').style.display = 'none';
+        }
+    });
+
+    //show comments
+    forEachComment( createComment );
+
+// button 
+
+$('#loginLink').onclick = function(){
+    $('#loginDiv').style.display = 'block';
+    $('#signupDiv').style.display = 'none';
+}
+
+$('#signupLink').onclick = function(){
+    $('#loginDiv').style.display = 'none';
+    $('#signupDiv').style.display = 'block';
+}
+
+$('#slattbratha').onclick = function(){
+    logout();
+}
+
+$('#loginBtn').onclick = function(){
+    login( $('#email').value, $('#password').value )
+    .catch( err => $('.error').innerText = err.message );
+}
+
+$('#registerBtn').onclick = function(){
+    signup( $('#emailReg').value, $('#passwordReg').value )
+    .catch( err => $('.error').innerText = err.message );
+}
+
+$('#addCommentBtn').onclick = function(){
+    addComment( $('#newComment').value )
+    .then( () => {
+        createComment({comment: $('#newComment').value});
+        $('#newComment').value = '';
+    })
+    .catch( err => $('.error').innerText = err.message )
+}
+
+
+}
