c823fe4db0bd4008459674c383cdf44a36aab6a3
Chervon Dawkins <chervon_dawkins@bloomfield.edu>
2021-11-15 15:42:43 -0500

    Add files via upload

--- firebase.mjs
+++ firebase.mjs
@@ -50,7 +50,7 @@ window.onLogin = function( f ){
 // exposed functionality for db
 window.addComment = function(comment){
     return addDoc( collection(db, "comments"), 
-        {comment, posted: serverTimestamp() } );
+        {comment } );
 }
 
 window.forEachComment = async function( f ){
